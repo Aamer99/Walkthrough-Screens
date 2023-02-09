69e429ee0a1bc353e16d2c38ad6693968f8ccff2
aamer Essa <aamer.es12@gmail.com>
2023-02-09 12:49:53 +0300

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Aamer Essa on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
